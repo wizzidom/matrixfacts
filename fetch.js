033473c09749524cb31002b65cd5a5eabd63e639
Wisdom <94754503+wisdomwiseman@users.noreply.github.com>
2022-12-08 11:18:12 +0200

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -3,20 +3,19 @@ let factText = document.querySelector('#factText');
 
 let numberInput = document.querySelector('#numberInput');
 // get number everytime theres certain input
-numberInput.addEventListener('input', getFactAjax);
+numberInput.addEventListener('input', getFactFetch);
 
-function getFactAjax(){
-    let number  = numberInput.value;
-    
 
-    let xhr  = new XMLHttpRequest();
-    xhr.open('GET','http://numbersapi.com/' + number);
-
-    xhr.onload = function(){
-        if(this.status == 200 && number != ''){
+    function getFactFetch(){
+        let number  = numberInput.value;
+        fetch('http://numbersapi.com/' + number)
+        .then(response => response.text())
+        .then(data=>{
+             if(number != ''){
             fact.style.display = 'block';
-            factText.innerText = this.responseText;
+            factText.innerText = data;
         }
+        
+        })
+        .catch(err => console.log(err))
     }
-    xhr.send();
-}    
